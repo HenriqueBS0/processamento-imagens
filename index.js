b22fb85a0862b56cce938d060998ba36583c6241
HenriqueBS0 <henrique.10agr@gmail.com>
2023-11-27 02:07:35 -0300

    histogram-multispectral

--- index.js
+++ index.js
@@ -2,16 +2,17 @@ const fs = require('fs');
 const ImageCreatorByContent = require('./class/ImageCreatorByContent');
 const ImageHistogramManipulator = require('./class/ImageHistogramManipulator');
 
-const execute = async () => {
-    const image1 = await ImageCreatorByContent.getFromTiff(fs.readFileSync('./img/Fig0316(1)(top_left).tif'));
-    const image2 = await ImageCreatorByContent.getFromTiff(fs.readFileSync('./img/Fig0316(2)(2nd_from_top).tif'));
-    const image3 = await ImageCreatorByContent.getFromTiff(fs.readFileSync('./img/Fig0316(3)(third_from_top).tif'));
-    const image4 = await ImageCreatorByContent.getFromTiff(fs.readFileSync('./img/Fig0316(4)(bottom_left).tif'));
+const fig1 = ImageCreatorByContent.getImage(fs.readFileSync('./img/Entrada_RGB.ppm', 'utf-8'));
+const fig2 = ImageCreatorByContent.getImage(fs.readFileSync('./img/Fig2.ppm', 'utf-8'));
 
-    fs.writeFileSync('./img/highlighting-by-histogram/fig1.pgm', ImageHistogramManipulator.highlighting(image1).getContent());
-    fs.writeFileSync('./img/highlighting-by-histogram/fig2.pgm', ImageHistogramManipulator.highlighting(image2).getContent());
-    fs.writeFileSync('./img/highlighting-by-histogram/fig3.pgm', ImageHistogramManipulator.highlighting(image3).getContent());
-    fs.writeFileSync('./img/highlighting-by-histogram/fig4.pgm', ImageHistogramManipulator.highlighting(image4).getContent());
-};
+const fig1EQ = ImageHistogramManipulator.equalize(fig1);
+const fig2EQ = ImageHistogramManipulator.equalize(fig2);
 
-execute();
+const fig1Realce = ImageHistogramManipulator.highlighting(fig1);
+const fig2Realce = ImageHistogramManipulator.highlighting(fig2);
+    
+fs.writeFileSync('./img/histogram-multispectral/fig1-equalizada.ppm', fig1EQ.getContent());
+fs.writeFileSync('./img/histogram-multispectral/fig2-equalizada.ppm', fig2EQ.getContent());
+
+fs.writeFileSync('./img/histogram-multispectral/fig1-realcada.ppm', fig1Realce.getContent());
+fs.writeFileSync('./img/histogram-multispectral/fig2-realcada.ppm', fig2Realce.getContent());
