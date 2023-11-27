8729c19c8f3dc6f45795329f857e680a0dd992fe
HenriqueBS0 <henrique.10agr@gmail.com>
2023-11-27 01:49:59 -0300

    highlighting-by-histogram-2

--- index.js
+++ index.js
@@ -1,33 +1,17 @@
 const fs = require('fs');
 const ImageCreatorByContent = require('./class/ImageCreatorByContent');
-const ImageBitPlanner = require('./class/ImageBitPlanner');
+const ImageHistogramManipulator = require('./class/ImageHistogramManipulator');
 
-const execute = async function() {
+const execute = async () => {
+    const image1 = await ImageCreatorByContent.getFromTiff(fs.readFileSync('./img/Fig0316(1)(top_left).tif'));
+    const image2 = await ImageCreatorByContent.getFromTiff(fs.readFileSync('./img/Fig0316(2)(2nd_from_top).tif'));
+    const image3 = await ImageCreatorByContent.getFromTiff(fs.readFileSync('./img/Fig0316(3)(third_from_top).tif'));
+    const image4 = await ImageCreatorByContent.getFromTiff(fs.readFileSync('./img/Fig0316(4)(bottom_left).tif'));
 
-    const image = await ImageCreatorByContent.getFromTiff(fs.readFileSync('./img/Fig0314(a)(100-dollars).tif'));
-
-    ImageBitPlanner.separete(image).forEach((image, planne) => {
-        fs.writeFileSync(`./img/image-bit-planner/dollars-planne-${planne+1}.pbm`, image.getContent());
-    });
-
-    const joinImage = ImageBitPlanner.join([
-        {
-            plane: 8,
-            image: ImageCreatorByContent.getImage(fs.readFileSync('./img/image-bit-planner/dollars-planne-8.pbm', 'utf-8')),
-        },
-
-        {
-            plane: 7,
-            image: ImageCreatorByContent.getImage(fs.readFileSync('./img/image-bit-planner/dollars-planne-7.pbm', 'utf-8')),
-        },
-
-        {
-            plane: 6,
-            image: ImageCreatorByContent.getImage(fs.readFileSync('./img/image-bit-planner/dollars-planne-6.pbm', 'utf-8')),
-        },
-    ]);
-
-    fs.writeFileSync('./img/image-bit-planner/join.pgm', joinImage.getContent());
+    fs.writeFileSync('./img/highlighting-by-histogram/fig1.pgm', ImageHistogramManipulator.highlighting(image1).getContent());
+    fs.writeFileSync('./img/highlighting-by-histogram/fig2.pgm', ImageHistogramManipulator.highlighting(image2).getContent());
+    fs.writeFileSync('./img/highlighting-by-histogram/fig3.pgm', ImageHistogramManipulator.highlighting(image3).getContent());
+    fs.writeFileSync('./img/highlighting-by-histogram/fig4.pgm', ImageHistogramManipulator.highlighting(image4).getContent());
 };
 
 execute();
